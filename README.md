bffe39e39b9efca84517be637edf3672df47c8e6
Oliver <ohardt@gmail.com>
2015-01-27 14:00:00 -0500

    initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# elasticsearch-test-data
-Generate and upload test data to Elasticsearch for eg. performance testing
+Elasticsearch For Beginners: Generate and Upload Randomized Test Data
+====
+Because everybody loves test data.
+
+
+### Ok, so what is this thing doing?
+
+`es_test_data.py` lets you generate and upload randomized test data to your ES cluster so you can start running queries, see what performance is like, and verify your cluster is able to handle the load.<br>
+It allows for easy configuring of what the test documents look like, what kind of data types they include and what the field names are called.
+
+## Cool, how do I use this? 
+
+#### Prerequisites
+
+Let's assume you have an Elasticsearch cluster running. If not, [set it up locally](http://ohardt.us/es-install) and point your browser to [http://localhost:9200](http://localhost:9200) to see if it's up.
+
+Python and [Tornado](https://github.com/tornadoweb/tornado/) are used, run `pip install tornado` to install Tornado if you don't have it already.
+
+#### Lets get started
+
+It's as simple as this:
+
+```
+$ python es_test_data.py
+[I 150126 22:26:44 es_test_data:40]  Trying to create index http://localhost:9200//test_data
+[I 150126 22:26:44 es_test_data:40]  Creating index "test_data" done   {"acknowledged":true}
+[I 150126 22:26:44 es_test_data:177] Generating 1000 docs, upload batch size is 500
+[I 150126 22:26:44 es_test_data:67]  Upload: OK - upload took:    26ms, total docs uploaded:     500
+[I 150126 22:26:44 es_test_data:67]  Upload: OK - upload took:    18ms, total docs uploaded:    1000
+[I 150126 22:26:44 es_test_data:201] Done - total count 1000, took 0 seconds
+$
+```
+ 
+Without any command line options, it will generate and upload 1000 documents of the format
+
+```
+{
+    "name":<<str>>,
+    "age":<<int>>,
+    "last_updated":<<ts>>
+}
+```
+to an Elasticsearch cluster at `http://localhost:9200` to an index called `test_data`.
+
+#### Not bad but what can I configure?
+
+`python es_test_data.py --help` gives you the full set of command line options, here are the most important ones:
+
+- `--es_url=http://localhost:9200` the base URL of your ES node, don't include the index name
+- `--count=###` number of documents to generate and upload
+- `--index_name=test_data` the name of the index to upload the data to. If it doesn't exist it'll be created with these options
+  - `--num_of_shards=2` the number of shards for the index
+  - `num_of_replicas=0` the number of replicas for the index
+- `--batch_size=###` we use bulk upload to send the docs to ES, this option controls how many we send at a time
+- `--force_init_index=False` if `True` it will delete and re-create the index
+- `--dict_file=filename.dic` if provided the `dict` data type will use words from the dictionary file, format is one word per line. The entire file is loaded at start-up so be careful with (very) large files. You can download wordlists e.g.. from [here](http://ohardt.us/word-lists). 
+
+#### What about the document format? 
+
+Glad you're asking, let's get to the doc format.<br>
+The doc format is configured via `--format=<<FORMAT>>` with the default being `name:str,age:int,last_updated:ts`.
+
+The general syntax looks like this:
+
+`<<field_name>>:<<field_type>>,<<field_name>>::<<field_type>>, ...`
+
+For every document, `es_test_data.py` will generate random values for each of the fields configured.
+
+Currently supported field types are:
+
+- `ts` a timestamp (in milliseconds), randomly picked between now +/- 30 days
+- `int:min:max` a random integer between `min` and `max`. If `min and `max` are not provided they default to 0 and 100000
+- `str:min:max` a word ( as in, a string), made up of `min` to `max` random upper/lowercase and digit characters. If `min` and `max` are optional, defaulting to `3` and `10`
+- `words:min:max` a random number of `strs`, separated by space, `min` and `max` are optional, defaulting to '2' and `10`
+- `dict:min:max` a random number of entries from the dictionary file, separated by space, `min` and `max` are optional, defaulting to '2' and `10`
+
+
+
+## todo
+- document the remaining cmd line options
+- more different format types
+- ...
+
+All suggestions, comments, ideas, pull requests are welcome!
+
---
+++ es_test_data.py
@@ -0,0 +1,228 @@
+from tornado.httpclient import HTTPClient, HTTPError, HTTPRequest
+from tornado.ioloop import IOLoop
+import tornado.options
+import json
+import time
+import logging
+import random
+import string
+import uuid
+
+
+http_client = HTTPClient()
+id_counter = 0
+
+
+def delete_index(idx_name):
+    try:
+        url = "%s/%s?refresh=true" % (tornado.options.options.es_url, idx_name)
+        request = HTTPRequest(url, method="DELETE", request_timeout=240)
+        response = http_client.fetch(request)
+        logging.info('Deleting index  "%s" done   %s' % (idx_name, response.body))
+    except HTTPError:
+        pass
+
+
+def create_index(idx_name):
+
+    schema = {
+        "settings": {
+            "number_of_shards": tornado.options.options.num_of_shards,
+            "number_of_replicas": tornado.options.options.num_of_replicas
+        },
+        "refresh": True
+    }
+
+    body = json.dumps(schema)
+    url = "%s/%s" % (tornado.options.options.es_url, idx_name)
+    try:
+        logging.info('Trying to create index %s' % (url))
+        request = HTTPRequest(url, method="PUT", body=body, request_timeout=240)
+        response = http_client.fetch(request)
+        logging.info('Creating index "%s" done   %s' % (idx_name, response.body))
+    except HTTPError:
+        pass
+
+
+def upload_batch(upload_data_txt):
+
+    request = HTTPRequest(tornado.options.options.es_url + "/_bulk", method="POST", body=upload_data_txt, request_timeout=240)
+    response = http_client.fetch(request)
+    result = json.loads(response.body)
+
+    res_txt = "OK" if not result['errors'] else "FAILED"
+    return res_txt, result['took']
+
+
+def get_data_for_format(format):
+    split_f = format.split(":")
+    if not split_f:
+        return None, None
+
+    field_name = split_f[0]
+    field_type = split_f[1]
+
+    if field_type == "str":
+        min = 3 if len(split_f) < 3 else int(split_f[2])
+        max = min + 7 if len(split_f) < 4 else int(split_f[3])
+        length = random.randrange(min, max)
+        return_val = "".join([random.choice(string.ascii_letters + string.digits) for x in range(length)])
+
+    elif field_type == "int":
+        min = 0 if len(split_f) < 3 else int(split_f[2])
+        max = min + 100000 if len(split_f) < 4 else int(split_f[3])
+        return_val = random.randrange(min, max)
+
+    elif field_type == "ts":
+        now = int(time.time())
+        per_day = 24 * 60 * 60
+        min = now - 30 * per_day if len(split_f) < 3 else int(split_f[2])
+        max = now + 30 * per_day if len(split_f) < 4 else int(split_f[3])
+        return_val = int(random.randrange(min, max) * 1000)
+
+    elif field_type == "words":
+        min = 2 if len(split_f) < 3 else int(split_f[2])
+        max = min + 8 if len(split_f) < 4 else int(split_f[3])
+        count = random.randrange(min, max)
+        words = []
+        for _ in range(count):
+            word_len = random.randrange(3, 10)
+            words.append("".join([random.choice(string.ascii_letters + string.digits) for x in range(word_len)]))
+        return_val = " ".join(words)
+
+    elif field_type == "dict":
+        global _dict_data
+        min = 2 if len(split_f) < 3 else int(split_f[2])
+        max = min + 8 if len(split_f) < 4 else int(split_f[3])
+        count = random.randrange(min, max)
+        return_val = " ".join([random.choice(_dict_data).strip() for _ in range(count)])
+
+    return field_name, return_val
+
+
+def generate_random_doc(format):
+    global id_counter
+
+    res = {}
+
+    for f in format:
+        f_key, f_val = get_data_for_format(f)
+        if f_key:
+            res[f_key] = f_val
+
+    if not tornado.options.options.id_type:
+        return res
+
+    if tornado.options.options.id_type == 'int':
+        res['_id'] = id_counter
+        id_counter += 1
+    elif tornado.options.options.id_type == 'uuid4':
+        res['_id'] = str(uuid.uuid4())
+
+    return res
+
+
+def set_index_refresh(val):
+
+    params = {"index": {"refresh_interval": val}}
+    body = json.dumps(params)
+    url = "%s/%s/_settings" % (tornado.options.options.es_url, tornado.options.options.index_name)
+    try:
+        request = HTTPRequest(url, method="PUT", body=body, request_timeout=240)
+        http_client.fetch(request)
+        logging.info('Set index refresh to %s' % val)
+    except HTTPError:
+        pass
+
+
+_dict_data = None
+
+
+def generate_test_data():
+
+    if tornado.options.options.force_init_index:
+        delete_index(tornado.options.options.index_name)
+
+    create_index(tornado.options.options.index_name)
+
+    # todo: query what refresh is set to, then restore later
+    if tornado.options.options.set_refresh:
+        set_index_refresh("-1")
+
+    if tornado.options.options.out_file:
+        out_file = open(tornado.options.options.out_file, "w")
+    else:
+        out_file = None
+
+    if tornado.options.options.dict_file:
+        global _dict_data
+        with open(tornado.options.options.dict_file, 'r') as f:
+            _dict_data = f.readlines()
+        logging.info("Loaded %d words from the %s" % (len(_dict_data), tornado.options.options.dict_file))
+
+    format = tornado.options.options.format.split(',')
+    if not format:
+        logging.error('invalid format')
+        exit(1)
+
+    ts_start = int(time.time())
+    upload_data_txt = ""
+    upload_data_count = 0
+    total_uploaded = 0
+
+    logging.info("Generating %d docs, upload batch size is %d" % (tornado.options.options.count, tornado.options.options.batch_size))
+    for num in range(0, tornado.options.options.count):
+
+        item = generate_random_doc(format)
+
+        if out_file:
+            out_file.write("%s\n" % json.dumps(item))
+
+        cmd = {'index': {'_index': tornado.options.options.index_name,
+                         '_type': tornado.options.options.index_type}}
+        if'_id' in item:
+            cmd['index']['_id'] = item['_id']
+
+        upload_data_txt += json.dumps(cmd) + "\n"
+        upload_data_txt += json.dumps(item) + "\n"
+        upload_data_count += 1
+
+        if upload_data_count == tornado.options.options.batch_size:
+            res_txt, took = upload_batch(upload_data_txt)
+            total_uploaded += upload_data_count
+            upload_data_txt = ""
+            upload_data_count = 0
+            logging.info("Upload: %s - upload took: %5dms, total docs uploaded: %7d" % (res_txt, took, total_uploaded))
+
+    # upload remaining items in `upload_data_txt`
+    if upload_data_txt:
+        upload_batch(upload_data_txt)
+
+    if tornado.options.options.set_refresh:
+        set_index_refresh("1s")
+
+    if out_file:
+        out_file.close()
+
+    took_secs = int(time.time() - ts_start)
+    logging.info("Done - total count %d, took %d seconds" % (tornado.options.options.count, took_secs))
+
+
+if __name__ == '__main__':
+    tornado.options.define("es_url", type=str, default='http://localhost:9200/', help="URL of your Elasticsearch node")
+    tornado.options.define("index_name", type=str, default='test_data', help="Name of the index to store your messages")
+    tornado.options.define("index_type", type=str, default='test_type', help="Type")
+    tornado.options.define("batch_size", type=int, default=500, help="Elasticsearch bulk index batch size")
+    tornado.options.define("num_of_shards", type=int, default=2, help="Number of shards for ES index")
+    tornado.options.define("count", type=int, default=1000, help="Number of docs to generate")
+    tornado.options.define("format", type=str, default='name:str,age:int,last_updated:ts', help="message format")
+    tornado.options.define("num_of_replicas", type=int, default=0, help="Number of replicas for ES index")
+    tornado.options.define("force_init_index", type=bool, default=False, help="Force deleting and re-initializing the Elasticsearch index")
+    tornado.options.define("set_refresh", type=bool, default=False, help="Set refresh rate to -1 before starting the upload")
+    tornado.options.define("out_file", type=str, default=False, help="If set, write test data to out_file as well.")
+    tornado.options.define("id_type", type=str, default=None, help="Type of 'id' to use for the docs, valid settings are int and uuid4, None is default")
+    tornado.options.define("dict_file", type=str, default=None, help="Name of dictionary file to use")
+
+    tornado.options.parse_command_line()
+
+    IOLoop.instance().run_sync(generate_test_data)
